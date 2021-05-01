37156eb010cd63d5714f74af63b04fad6071bbb5
Benjamin <bezett21@gmail.com>
2021-05-01 21:30:44 +0200

    Klasse Fach hinzugefügt

---
+++ src/backend/Fach.java
@@ -0,0 +1,25 @@
+package backend;
+
+public class Fach
+{
+    public Fach(String bezeichnung, int anzahlFragen)
+    {
+        this.bezeichnung = bezeichnung;
+        this.anzahlFragen = anzahlFragen;
+    }
+
+    
+
+    public String getBezeichnung()
+    {
+        return bezeichnung;
+    }
+
+    public int getAnzahlFragen()
+    {
+        return anzahlFragen;
+    }
+
+    private final String bezeichnung;
+    private final int anzahlFragen;
+}
