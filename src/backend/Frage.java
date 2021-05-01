de04da3b9ceac04d96379c504bc7b30a83c8207b
Benjamin <bezett21@gmail.com>
2021-05-01 21:23:07 +0200

    Klasse Frage hinzugefügt

---
+++ src/backend/Frage.java
@@ -0,0 +1,58 @@
+package backend;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Frage
+{
+    public static enum Schwierigkeit
+    {
+        leicht,
+        mittel,
+        schwer
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<String> liste = new ArrayList<String>();
+        liste.add("1");
+        liste.add("2");
+        liste.add("3");
+        liste.add("4");
+        Frage frage = new Frage("hallo?", "ja", liste, "meh", Schwierigkeit.schwer);
+        for (String string : frage.getAlleAntworten())
+        {
+            System.out.println(string);
+        }
+    }
+
+    public Frage(String frage, String richtigeAntwort, ArrayList<String> falscheAntworten, String fach, Schwierigkeit schwierigkeit)
+    {
+        this.frage = frage;
+        this.richtigeAntwort = richtigeAntwort;
+        this.falscheAntworten = falscheAntworten;
+        this.fach = fach;
+        this.schwierigkeit = schwierigkeit;
+    }
+
+    public ArrayList<String> getAlleAntworten()
+    {
+        ArrayList<String> alleAntworten = falscheAntworten;
+        alleAntworten.add(richtigeAntwort);
+        Collections.shuffle(alleAntworten);
+        return alleAntworten;
+    }
+
+    public String getFrage() { return frage; }
+    public String getRichtigeAntwort() { return richtigeAntwort;}
+    public ArrayList<String> getFalscheAntworten() { return falscheAntworten; }
+    public String getFach() { return fach; }
+    public Schwierigkeit getSchwierigkeit() { return schwierigkeit; }
+
+    private final String frage;
+    private final String richtigeAntwort;
+    private final ArrayList<String> falscheAntworten;
+    private final String fach;
+    private final Schwierigkeit schwierigkeit;
+
+}
