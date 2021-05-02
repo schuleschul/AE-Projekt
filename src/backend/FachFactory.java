3d196c389858aba3b72d17739832d738843370d7
Benjamin <bezett21@gmail.com>
2021-05-02 12:32:56 +0200

    FachFactory hinzugefügt

---
+++ src/backend/FachFactory.java
@@ -0,0 +1,26 @@
+package backend;
+import datenbank.DatenbankInterface;
+import java.util.ArrayList;
+
+public class FachFactory
+{
+    public FachFactory(DatenbankInterface datenbank)
+    {
+        this.datenbank = datenbank;
+    }
+
+    public ArrayList<Fach> laden(FachSuchkriterium suchkriterium)
+    {
+        return datenbank.laden(suchkriterium);
+    }
+
+    //lädt einfach alle Fächer
+    public ArrayList<Fach> laden()
+    {
+        FachSuchkriterium suchkriterium = new FachSuchkriterium();
+        return datenbank.laden(suchkriterium);
+    }
+
+
+    private final DatenbankInterface datenbank;
+}
