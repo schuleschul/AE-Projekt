ad166703dce0dac4b2a27e56cb07b05c2c52cdc5
Benjamin <bezett21@gmail.com>
2021-05-02 12:32:21 +0200

    Definitionen der Methoden für DatenbankInterface hinzugefügt (vom Implementierer der Klasse gerne zu verändern)

---
+++ src/datenbank/DatenbankInterface.java
@@ -0,0 +1,36 @@
+package datenbank;
+
+import backend.Fach;
+import backend.FachSuchkriterium;
+import backend.Frage;
+import backend.FragenSuchkriterium;
+
+import java.util.ArrayList;
+
+public class DatenbankInterface
+{
+    public DatenbankInterface(String sqliteDatenbankPfad)
+    {
+
+    }
+
+    public ArrayList<Fach> laden(FachSuchkriterium suchkriterium)
+    {
+
+    }
+
+    public ArrayList<Frage> laden(FragenSuchkriterium suchkriterium)
+    {
+
+    }
+
+    public void speichern(Frage frage)
+    {
+
+    }
+
+    public void speichern(Fach fach)
+    {
+
+    }
+}
