7a1416188fd66441283b8a56bd0b931af4910e77
Nadya Bissness <noreply@gofuckyourself.com>
2021-05-08 17:12:43 +0200

    Frage an die neue Anforderung angepasst

--- src/backend/Frage.java
+++ src/backend/Frage.java
@@ -12,27 +12,30 @@ public class Frage
         schwer
     }
 
-    public static void main(String[] args)
-    {
-        ArrayList<String> liste = new ArrayList<String>();
-        liste.add("1");
-        liste.add("2");
-        liste.add("3");
-        liste.add("4");
-        Frage frage = new Frage("hallo?", "ja", liste, "meh", Schwierigkeit.schwer);
-        for (String string : frage.getAlleAntworten())
-        {
-            System.out.println(string);
-        }
-    }
+//    public static void main(String[] args)
+//    {
+//        ArrayList<String> liste = new ArrayList<String>();
+//        liste.add("1");
+//        liste.add("2");
+//        liste.add("3");
+//        liste.add("4");
+//        Frage frage = new Frage("hallo?", "ja", liste, "meh", Schwierigkeit.schwer);
+//        for (String string : frage.getAlleAntworten())
+//        {
+//            System.out.println(string);
+//        }
+//    }
 
-    public Frage(String frage, String richtigeAntwort, ArrayList<String> falscheAntworten, String fach, Schwierigkeit schwierigkeit)
+    public Frage(String frage, String richtigeAntwort, ArrayList<String> falscheAntworten, String fach, Schwierigkeit schwierigkeit,
+                  int id, int anzahlRichtigBeantwortet)
     {
         this.frage = frage;
         this.richtigeAntwort = richtigeAntwort;
         this.falscheAntworten = falscheAntworten;
         this.fach = fach;
         this.schwierigkeit = schwierigkeit;
+        this.id = id;
+        this.anzahlRichtigBeantwortet = anzahlRichtigBeantwortet;
     }
 
     public ArrayList<String> getAlleAntworten()
@@ -48,11 +51,15 @@ public class Frage
     public ArrayList<String> getFalscheAntworten() { return falscheAntworten; }
     public String getFach() { return fach; }
     public Schwierigkeit getSchwierigkeit() { return schwierigkeit; }
+    public int getId() { return id; }
+    public int getAnzahlRichtigBeantwortet() {return anzahlRichtigBeantwortet; }
 
     private final String frage;
     private final String richtigeAntwort;
     private final ArrayList<String> falscheAntworten;
     private final String fach;
     private final Schwierigkeit schwierigkeit;
+    private final int id;
+    private final int anzahlRichtigBeantwortet;
 
 }
