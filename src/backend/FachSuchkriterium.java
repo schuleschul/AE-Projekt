8279f04da16b8c67f28c48964eb2ce447c4bef8e
schuleschul <nadyabissness@gofuckyourself.com>
2021-05-08 18:30:56 +0200

    ID in FachSuchkriterium hinzugefügt

--- src/backend/FachSuchkriterium.java
+++ src/backend/FachSuchkriterium.java
@@ -6,6 +6,9 @@ public class FachSuchkriterium
 
     public String getBezeichnung() { return bezeichnung; }
     public void setBezeichnung(String bezeichnung) { this.bezeichnung = bezeichnung; }
+    public Integer getId() { return id; }
+    public void setId(int id) {this.id = id; }
 
     private String bezeichnung = null;
+    private Integer id = null;
 }
